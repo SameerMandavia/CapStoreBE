78b93d85929afe0a4816243a31f797ae7794772b
SameerMandavia <60601676+SameerMandavia@users.noreply.github.com>
2021-08-06 14:42:48 +0530

    Added Project Structure

---
+++ CapStoreBE/src/main/java/com/capstore/CapStoreBeApplication.java
@@ -0,0 +1,13 @@
+package com.capstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapStoreBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapStoreBeApplication.class, args);
+	}
+
+}
---
+++ CapStoreBE/src/test/java/com/capstore/CapStoreBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapStoreBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
